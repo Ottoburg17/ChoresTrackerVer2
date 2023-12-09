d5e1641cb416b1b2f2929ca93206e104ce393544
Nemeth Otto <ottoburg17@gmail.com>
2023-12-09 12:05:32 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const loginWindow = document.getElementById("loginWindow");
 const signupBtn = document.getElementById("signupBtn");
 const signupWindow = document.getElementById("signupWindow");
 const closeBtn = document.getElementById("xBtn");
+const closeBtn2 = document.getElementById("xBtn2");
 
 loginBtn.addEventListener('click', start);
 closeBtn.addEventListener('click', stop );
@@ -19,10 +20,14 @@ function stop(){
 
 
 
+
 signupBtn.addEventListener('click', xy);
+closeBtn2.addEventListener('click', yx );
 
 function xy(){
     signupWindow.style.display= "flex";
 }
 
-
+function yx(){
+    signupWindow.style.display= "none";
+}
